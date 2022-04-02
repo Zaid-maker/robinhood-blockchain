fdde329361f20ffbd2d8ed8805fcd0aac315a61b
DevMirza <53424436+Zaid-maker@users.noreply.github.com>
2022-04-02 23:00:49 +0500

    intial commit

---
+++ components/BuyTokens.js
@@ -0,0 +1,7 @@
+import React from "react";
+
+const BuyTokens = () => {
+  return <div>BuyTokens</div>;
+};
+
+export default BuyTokens;
---
+++ components/PortfolioChart.js
@@ -0,0 +1,7 @@
+import React from "react";
+
+const PortfolioChart = () => {
+  return <div>PortfolioChart</div>;
+};
+
+export default PortfolioChart;
