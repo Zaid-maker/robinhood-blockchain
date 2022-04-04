cedc4367b9450ef758acde6bc224cdf47c089d0f
DevMirza <53424436+Zaid-maker@users.noreply.github.com>
2022-04-05 00:05:58 +0500

    fix create user

--- pages/api/createUsser.js
+++ pages/api/createUsser.js
@@ -2,12 +2,19 @@ import { client } from "../../lib/sanityClient";
 
 const createUser = async (req, res) => {
   try {
+    console.log("CREATING USER");
     const userDoc = {
       _type: "users",
       _id: req.body.walletAddress,
       userName: "Unnamed",
       address: req.body.walletAddress,
     };
+
+    await client.createIfNotExists(userDoc);
+
+    res.status(201).send({
+      message: "User created",
+    });
   } catch (err) {
     res.status(500).send({
       message: "Error creating user",
@@ -15,3 +22,5 @@ const createUser = async (req, res) => {
     });
   }
 };
+
+export default createUser;
