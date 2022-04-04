476eb42783aa494043281390240dc06a11583778
DevMirza <53424436+Zaid-maker@users.noreply.github.com>
2022-04-04 23:25:08 +0500

    moralis auth

--- context/RobinhoodContext.js
+++ context/RobinhoodContext.js
@@ -1,7 +1,59 @@
 import React, { createContext, useState, useEffect } from "react";
+import { useMoralis } from "react-moralis";
 
 export const RobinhoodContext = createContext();
 
 export const RobinhoodProvider = ({ children }) => {
-  return <RobinhoodContext.Provider>{children}</RobinhoodContext.Provider>;
+  const [currentAccount, setCurrentAccount] = useState();
+  const [formattedAccount, setFormattedAccount] = useState();
+
+  const { isAuthenticated, authenticate, user, logout, Moralis, enableWeb3 } =
+    useMoralis();
+
+  useEffect(() => {
+    if (isAuthenticated) {
+      const account = user.get("ethAddress");
+      let formatAccount = account.slice(0, 4) + "..." + account.slice(-4);
+      setFormattedAccount(formatAccount);
+      setCurrentAccount(account);
+    }
+  }, [isAuthenticated, enableWeb3]);
+
+  useEffect(() => {
+    if (!currentAccount) return;
+    (async () => {
+      const response = await fetch("/api/createUser", {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify({
+          walletAddress: currentAccount,
+        }),
+      });
+      const data = await response.json();
+    })();
+  }, [currentAccount]);
+
+  const connectWallet = async () => {
+    authenticate();
+  };
+
+  const signOut = () => {
+    logout();
+  };
+
+  return (
+    <RobinhoodContext.Provider
+      value={{
+        connectWallet,
+        signOut,
+        currentAccount,
+        isAuthenticated,
+        formattedAccount,
+      }}
+    >
+      {children}
+    </RobinhoodContext.Provider>
+  );
 };
--- pages/_app.js
+++ pages/_app.js
@@ -1,11 +1,17 @@
 import "../styles/globals.css";
 import { RobinhoodProvider } from "../context/RobinhoodContext";
+import { MoralisProvider } from "react-moralis";
 
 function MyApp({ Component, pageProps }) {
   return (
-    <RobinhoodProvider>
-      <Component {...pageProps} />
-    </RobinhoodProvider>
+    <MoralisProvider
+      appId="1gC7R9hXyKV3aaP8Vs1qxrDN1qXbgsh4568q9gSM"
+      serverUrl="https://c4lbhpfs7ydm.usemoralis.com:2053/server"
+    >
+      <RobinhoodProvider>
+        <Component {...pageProps} />
+      </RobinhoodProvider>
+    </MoralisProvider>
   );
 }
 
